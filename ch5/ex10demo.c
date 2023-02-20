6b127974b89c8677e92627ca1cec90be427e1226
Balor <122012816+WitsanuP@users.noreply.github.com>
2023-02-20 20:38:30 +0700

    Update ex10demo.c

--- ch5/ex10demo.c
+++ ch5/ex10demo.c
@@ -1,25 +1,41 @@
-#include<stdio.h>//what the fuck is shit
+#include<stdio.h>
 
-float root (float a){
-    float x=a;
-    float y=0;
-    while(x-y>0.000001){
-        y=a/x;
-        x=(x+y)/2;
-    }
-    return x;
-}
-int main(){
-    int a,b,c;
-    printf("a :");
-    scanf("%d",&a);
-    printf("b :");
-    scanf("%d",&b);
-    printf("c :");
-    scanf("%d",&c);
+int main() {
+    char line[50];
+    float a, b, c;
+    float num;
     
+    printf("a : ");
+    fgets(line,sizeof(line),stdin);
+    sscanf(line,"%f",&a);
 
-    printf("ans is %f")
+    printf("b : ");
+    fgets(line,sizeof(line),stdin);
+    sscanf(line,"%f",&b);
 
+    printf("c : ");
+    fgets(line,sizeof(line),stdin);
+    sscanf(line,"%f",&c);
 
+    num = b * b - 4 * a * c;
+    
+    float x = num;
+    float y = 0;
+    switch (num>=0)
+    {
+    case 0:
+        printf("WTF, I can't solve this question.");
+        break;
+
+    case 1:
+        while(x-y>0.00001){
+            y=num/x;
+            x=(x+y)/2;
+        }
+        float ans,ans2;
+        ans = ((-b) + x)/(2*a);
+        ans2 = ((-b) - x)/(2*a);
+        ans==ans2?printf("ans is %f",ans):printf("ans is %f %f",ans,ans2);
+        break;
+    }
 }
